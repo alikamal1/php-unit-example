7fd74bd15eac52b1ff8ee2408bb11286f0c044eb
Ali Kamal <alikamal1@yahoo.com>
2020-03-09 15:25:56 +0300

    array has key

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -28,4 +28,37 @@ class UserTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($user->getFullName(), "Ali Kamal");
     }
 
+    public function testFirstAndLastNameAreTrimmed()
+    {
+        $user = new \App\Models\User;
+        $user->setFirstName(" Ali       ");
+        $user->setLastName("        Kamal   ");
+
+        $this->assertEquals($user->getFirstName(), "Ali");
+        $this->assertEquals($user->getLastName(), "Kamal");
+    }
+
+    public function testEmailAddressCanBeSet()
+    {
+        $email = 'email@example.com';
+        $user = new \App\Models\User;
+        $user->setEmail($email);
+
+        $this->assertEquals($user->getEmail(), $email);
+    }
+
+    public function testEmailVariablesContainCorrectValues()
+    {
+        $user = new \App\Models\User;
+        $user->setFirstName("Ali");
+        $user->setLastName("Kamal");
+        $user->setEmail('email@example.com');
+        $emailVariables = $user->getEmailVariables();
+
+        $this->assertArrayHasKey('full_name', $emailVariables);
+        $this->assertArrayHasKey('email', $emailVariables);
+        $this->assertEquals($emailVariables['full_name'], 'Ali Kamal');
+        $this->assertEquals($emailVariables['email'], 'email@example.com');
+    }
+
 }
